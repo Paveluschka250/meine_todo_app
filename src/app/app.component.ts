ee9df336ff98e303f19126c7de24febc69d97a8b
Paul Gross <p.gross6408@gmail.com>
2025-10-01 21:02:59 +0200

    feat: Add todo list with data binding and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'meine_todo_app';
+  todoItems = todos;
 }
+
+interface TodoItem {
+  id: number;        // Eindeutige Nummer
+  text: string;      // Der Text der Aufgabe
+  completed: boolean; // Ist erledigt oder nicht?
+}
+
+let todos: TodoItem[] = [
+  { id: 1, text: 'Angular lernen', completed: false },
+  { id: 2, text: 'Todo-App bauen', completed: false },
+  { id: 3, text: 'JavaScript üben', completed: true }
+];
